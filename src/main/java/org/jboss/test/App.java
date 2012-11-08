7e00ad4ab115696ab4610ee2a4d6a5b9b14fe3f5
Josef Cacek <josef.cacek@gmail.com>
2012-11-08 07:36:42 +0100

    initial commit

---
+++ src/main/java/org/jboss/test/App.java
@@ -0,0 +1,23 @@
+package org.jboss.test;
+
+/**
+ * Hello world!
+ * 
+ * @author Josef Cacek
+ */
+public class App {
+
+    // Constructors ----------------------------------------------------------
+
+    // Public methods --------------------------------------------------------
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    // Protected methods -----------------------------------------------------
+
+    // Private methods -------------------------------------------------------
+
+    // Embedded classes ------------------------------------------------------
+}
