393d4d712f500bde6354ec4687094974bedc7751
Josef Cacek <josef.cacek@gmail.com>
2012-12-04 13:34:11 +0100

    play with passwords

--- src/main/java/org/jboss/test/App.java
+++ src/main/java/org/jboss/test/App.java
@@ -20,10 +20,10 @@ public class App {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         //        final Server server = Server.createTcpServer("-tcpAllowOthers").start();
         Class.forName("org.h2.Driver");
-        //        final String dbUrl = "jdbc:h2:mem:PicketLinkTest;DB_CLOSE_DELAY=-1";
-        final String dbUrl = "jdbc:h2:tcp://localhost/mem:PicketLinkTest";
+        final String dbUrl = "jdbc:h2:mem:PicketLinkTest;DB_CLOSE_DELAY=-1";
+        //        final String dbUrl = "jdbc:h2:tcp://localhost/mem:PicketLinkTest";
 
-        final Connection conn = DriverManager.getConnection(dbUrl, "sa", "sa");
+        final Connection conn = DriverManager.getConnection(dbUrl, "sa", "xxx");
         PreparedStatement ps = conn.prepareStatement("SELECT ? FROM VALUES('1') V");
         ps.setString(1, "Hello");
         ResultSet rs = ps.executeQuery();
@@ -32,6 +32,19 @@ public class App {
         }
         rs.close();
         ps.close();
+        //        conn.close();
+
+        final Connection conn2 = DriverManager.getConnection(dbUrl, "sa", "yyy");
+        ps = conn2.prepareStatement("SELECT ? FROM VALUES('1') V");
+        ps.setString(1, "Hello");
+        rs = ps.executeQuery();
+        while (rs.next()) {
+            System.out.println(rs.getString(1));
+        }
+        rs.close();
+        ps.close();
+
+        conn2.close();
         conn.close();
         //        server.stop();
     }
