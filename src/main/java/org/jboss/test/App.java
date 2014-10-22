15a9bb52e3b36835b855c08770a5f334d39c980e
Josef Cacek <josef.cacek@gmail.com>
2014-10-22 16:09:13 +0200

    hostnames test

--- src/main/java/org/jboss/test/App.java
+++ src/main/java/org/jboss/test/App.java
@@ -1,5 +1,8 @@
 package org.jboss.test;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 /**
  * Hello world!
  * 
@@ -7,8 +10,27 @@ package org.jboss.test;
  */
 public class App {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+  public static void main(String[] args) {
+    if (args.length == 0) {
+      System.out.println("Usage:");
+      System.out.println("\tjava -jar host-test.jar <IP|hostname> ...");
+      System.out.println();
+      System.exit(1);
     }
+    for (String arg : args) {
+      System.out.println("[ " + arg + " ]");
+      try {
+        InetAddress[] addrs = InetAddress.getAllByName(arg);
+        for (InetAddress addr : addrs) {
+          System.out.println("getHostAddress():       " + addr.getHostAddress());
+          System.out.println("getHostName():          " + addr.getHostName());
+          System.out.println("getCanonicalHostName(): " + addr.getCanonicalHostName());
+          System.out.println();
+        }
+      } catch (UnknownHostException e) {
+        e.printStackTrace();
+      }
+    }
+  }
 
 }
