51c0d85f3b3a038602bcd1ff2c8037e9902de0f5
Josef Cacek <josef.cacek@gmail.com>
2015-04-20 22:22:51 +0200

    JBoss LogManager and JBoss Logging

--- src/main/java/org/jboss/test/App.java
+++ src/main/java/org/jboss/test/App.java
@@ -1,14 +1,28 @@
 package org.jboss.test;
 
+import org.jboss.logmanager.Logger;
+
 /**
- * Hello world!
+ * Sample usage of JBoss LogManager and JBoss Logging
  * 
  * @author Josef Cacek
  */
 public class App {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+	static {
+		System.setProperty("java.util.logging.manager", "org.jboss.logmanager.LogManager");
+	}
 
+	private static final org.jboss.logging.Logger LOGGER = org.jboss.logging.Logger.getLogger(App.class);
+	private static final Logger LOGGER2 = Logger.getLogger(App.class.getName());
+
+	public static void main(String[] args) {
+		LOGGER.debug("Test debug");
+		LOGGER.info("Test info");
+		LOGGER.warn("Test warn");
+
+		LOGGER2.fine("test fine");
+		LOGGER2.info("test info");
+		LOGGER2.warning("test warning");
+	}
 }
