b5f7a02f90a9dc93dd86faaf31bcf728fb1cf139
Josef Cacek <josef.cacek@gmail.com>
2015-06-17 11:31:58 +0200

    JBoss CLI memory leak reproducer

--- README.md
+++ README.md
@@ -1,5 +1,25 @@
-# Test application
+# Reproducer for JBoss CLI memory leak
 
-Java application playground
+The `org.jboss.as.cli.impl.CommandContextImpl` class calls the `initJaasConfig()` in its constructor, 
+which adds a new `JaasConfigurationWrapper` instance to `javax.security.auth.login.Configuration`.
+It's done for every new `CommandContext` instance, so it consumes more and more memory. There is no cleanup for the registered 
+configurations.
 
-MD Syntax guide - http://markdoc.org/ref/markup
+Moreover, the searches for appropriate 
+login config take more and more time because they have to go through all the wrapped objects.
+
+## How does the reproducer works
+
+It simply creates new `CommandContextImpl` instances:
+
+	counter=0;
+    while (true) {
+      CommandContextFactory.getInstance().newCommandContext();
+      counter++;
+    }
+
+## How to run it
+
+Use the Maven, Luke!
+
+    mvn clean package exec:java
--- src/main/java/org/jboss/test/App.java
+++ src/main/java/org/jboss/test/App.java
@@ -1,14 +1,34 @@
 package org.jboss.test;
 
+import java.io.File;
+import java.io.PrintWriter;
+
+import org.jboss.as.cli.CommandContextFactory;
+
 /**
- * Hello world!
- * 
+ * Reproducer for JBoss CLI memory leak.
+ *
  * @author Josef Cacek
  */
 public class App {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+  public static void main(String[] args) throws Exception {
+    // workaround for WARN messages on System.err
+    final File tmpJBossCliFile = File.createTempFile("jboss-cli", ".xml");
+    try (PrintWriter pw = new PrintWriter(tmpJBossCliFile)) {
+      pw.print("<jboss-cli xmlns='urn:jboss:cli:1.0'></jboss-cli>");
     }
+    System.setProperty("jboss.cli.config", tmpJBossCliFile.getAbsolutePath());
 
+    long counter = 0L;
+    long nextPrint = 1L;
+    while (true) {
+      CommandContextFactory.getInstance().newCommandContext().terminateSession();
+      counter++;
+      if (counter >= nextPrint) {
+        System.out.println("CommandContext clients created: " + counter);
+        nextPrint = nextPrint * 2L;
+      }
+    }
+  }
 }
