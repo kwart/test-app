63457f7c8f6430dd90647acca998ae9a16595a6d
Josef Cacek <josef.cacek@gmail.com>
2015-12-10 15:14:23 +0100

    Example why it's not a good idea to use string tokenizer to parse attributes from DN

--- README.md
+++ README.md
@@ -1,5 +1,80 @@
-# Test application
+# Attribute parsing from DN in LDAP
 
-Java application playground
+What's wrong with this role-name parsing from DN?
 
-MD Syntax guide - http://markdoc.org/ref/markup
+```java
+private static String parseRole(String dn, String groupNameAttribute) {
+	StringTokenizer st = new StringTokenizer(dn, ",");
+	while (st != null && st.hasMoreTokens()) {
+		String keyVal = st.nextToken();
+		if (keyVal.indexOf(groupNameAttribute) > -1) {
+			StringTokenizer kst = new StringTokenizer(keyVal, "=");
+			kst.nextToken();
+			String simpleName = kst.nextToken();
+			return simpleName;
+		}
+	}
+	return null;
+}
+```
+
+## Why not to use "key=value".indexOf(attribute)?
+
+Because user gets false positives.
+
+## Why not to use StringTokenizer to parse role names from DN?
+
+Because it ignores escaping in LDAP names.
+
+## Possible solution
+
+Use `javax.naming.ldap.LdapName` which implements [RFC-2253](https://tools.ietf.org/html/rfc2253).
+
+```java
+private static String parseRoleNew(String dn, String groupNameAttribute) {
+	try {
+		LdapName ldapName = new LdapName(dn);
+		for (int i = ldapName.size() - 1; i >= 0; i--) {
+			String rdnString = ldapName.get(i);
+			Rdn rdn = new Rdn(rdnString);
+			Attribute attr = rdn.toAttributes().get(groupNameAttribute);
+			if (attr != null) {
+				Object value = attr.get();
+				if (value != null) {
+					return (value instanceof byte[]) ? new String((byte[]) value) : value.toString();
+				}
+			}
+		}
+	} catch (NamingException e) {
+		e.printStackTrace();
+	}
+	return null;
+}
+```
+
+## Example
+
+Run [App.java](src/main/java/org/jboss/test/App.java) class. Here is the output:
+
+```
+Parsing role from dn cn=double,ou=admin for attribute ou
+Original impl: double
+     New impl: admin
+
+Parsing role from dn CN=L. Eagle,O=Sue\, Grabbit and Runn,C=GB for attribute O
+Original impl: Sue\
+     New impl: Sue, Grabbit and Runn
+
+Parsing role from dn OU=Sales+CN=J. Smith,O=Widget Inc.,C=US for attribute OU
+Original impl: Sales+CN
+     New impl: Sales
+
+Parsing role from dn CN=Before\0DAfter,O=Test,C=GB for attribute CN
+Original impl: Before\0DAfter
+     New impl: Before
+After
+
+Parsing role from dn OU=#4869,O=Test,C=GB for attribute OU
+Original impl: #4869
+     New impl: Hi
+```
--- src/main/java/org/jboss/test/App.java
+++ src/main/java/org/jboss/test/App.java
@@ -1,14 +1,69 @@
 package org.jboss.test;
 
+import java.util.StringTokenizer;
+
+import javax.naming.NamingException;
+import javax.naming.directory.Attribute;
+import javax.naming.ldap.LdapName;
+import javax.naming.ldap.Rdn;
+
 /**
  * Hello world!
- * 
+ *
  * @author Josef Cacek
  */
 public class App {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+	public static void main(String[] args) {
+		// wrong attribute name
+		compareRoleParsing("cn=double,ou=admin", "ou");
 
+		// escaping example from RFC 2253 - Section 5 -
+		// https://tools.ietf.org/html/rfc2253
+		compareRoleParsing("CN=L. Eagle,O=Sue\\, Grabbit and Runn,C=GB", "O");
+		compareRoleParsing("OU=Sales+CN=J. Smith,O=Widget Inc.,C=US", "OU");
+		compareRoleParsing("CN=Before\\0DAfter,O=Test,C=GB", "CN");
+		compareRoleParsing("OU=#4869,O=Test,C=GB", "OU");
+	}
+
+	private static void compareRoleParsing(String dn, String groupNameAttribute) {
+		System.out.println(String.format("Parsing role from dn %s for attribute %s", dn, groupNameAttribute));
+		System.out.println("Original impl: " + parseRole(dn, groupNameAttribute));
+		System.out.println("     New impl: " + parseRoleNew(dn, groupNameAttribute));
+		System.out.println();
+	}
+
+	private static String parseRole(String dn, String groupNameAttribute) {
+		StringTokenizer st = new StringTokenizer(dn, ",");
+		while (st != null && st.hasMoreTokens()) {
+			String keyVal = st.nextToken();
+			if (keyVal.indexOf(groupNameAttribute) > -1) {
+				StringTokenizer kst = new StringTokenizer(keyVal, "=");
+				kst.nextToken();
+				String simpleName = kst.nextToken();
+				return simpleName;
+			}
+		}
+		return null;
+	}
+
+	private static String parseRoleNew(String dn, String groupNameAttribute) {
+		try {
+			LdapName ldapName = new LdapName(dn);
+			for (int i = ldapName.size() - 1; i >= 0; i--) {
+				String rdnString = ldapName.get(i);
+				Rdn rdn = new Rdn(rdnString);
+				Attribute attr = rdn.toAttributes().get(groupNameAttribute);
+				if (attr != null) {
+					Object value = attr.get();
+					if (value != null) {
+						return (value instanceof byte[]) ? new String((byte[]) value) : value.toString();
+					}
+				}
+			}
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 }
