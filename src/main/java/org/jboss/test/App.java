f408bd0a4a2b5cd8c3b71646b46d44da03576815
Josef Cacek <josef.cacek@gmail.com>
2017-01-10 18:00:03 +0100

    Reproducer for JMS client JNDI lookup issue

--- src/main/java/org/jboss/test/App.java
+++ src/main/java/org/jboss/test/App.java
@@ -1,14 +1,51 @@
 package org.jboss.test;
 
+import java.util.Properties;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
 /**
  * Hello world!
- * 
+ *
  * @author Josef Cacek
  */
 public class App {
 
+    // Set up all the default values
+    private static final String DEFAULT_CONNECTION_FACTORY = "jms/RemoteConnectionFactory";
+    private static final String DEFAULT_USERNAME = "user1";
+    private static final String DEFAULT_PASSWORD = "password1!";
+    private static final String INITIAL_CONTEXT_FACTORY = "org.jboss.naming.remote.client.InitialContextFactory";
+    private static final String PROVIDER_URL = "http-remoting://172.17.0.2:8080";
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Context namingContext = null;
+
+        try {
+            String userName = System.getProperty("username", DEFAULT_USERNAME);
+            String password = System.getProperty("password", DEFAULT_PASSWORD);
+
+            // Set up the namingContext for the JNDI lookup
+            final Properties env = new Properties();
+            env.put(Context.INITIAL_CONTEXT_FACTORY, INITIAL_CONTEXT_FACTORY);
+            env.put(Context.PROVIDER_URL, System.getProperty(Context.PROVIDER_URL, PROVIDER_URL));
+            env.put(Context.SECURITY_PRINCIPAL, userName);
+            env.put(Context.SECURITY_CREDENTIALS, password);
+            namingContext = new InitialContext(env);
+            namingContext.lookup(DEFAULT_CONNECTION_FACTORY);
+        } catch (NamingException e) {
+            e.printStackTrace();
+        } finally {
+            if (namingContext != null) {
+                try {
+                    namingContext.close();
+                } catch (NamingException e) {
+                }
+            }
+        }
     }
 
 }
