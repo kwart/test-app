2ccd2e8147490ce97247841bfcac6330007652ff
Josef Cacek <josef.cacek@gmail.com>
2017-01-10 18:00:03 +0100

    Anonymous JMS client demo

--- README.md
+++ README.md
@@ -1,5 +1,34 @@
-# Test application
+# Demo - Use JMS client anonymous access with Elytron
 
-Java application playground
+This demo shows how to use use the `ANONYMOUS` SASL authentication mechanism from Elytron as replacement for `unauthenticatedIdentity` login module option in WidlFly legacy security.
 
-MD Syntax guide - http://markdoc.org/ref/markup
+We want to send a message from JMS client without authentication, so we want to allow anonymous access and grant `"guest"` role (used in messaging-activemq configuration) to incoming clients.
+
+[The client code](src/main/java/org/jboss/test/App.java) in this demo is based on [helloworld-jms](https://github.com/wildfly/quickstart/tree/11.x/helloworld-jms) WildFly quickstart.
+
+## Configure the Application server
+
+The configuration JBoss CLI script [demo.cli](demo.cli) contains commands to configure the server.
+
+```bash
+export JBOSS_HOME=/path/to/your/wildflyOrEap
+$JBOSS_HOME/bin/jboss-cli.sh --file=demo.cli
+```
+
+## Client configuration
+
+Custom [wildfly-config.xml](src/main/resources/wildfly-config.xml) is used to allow all SASL mechanisms.
+
+## Run the demo
+
+### Start the server (full profile)
+```bash
+$JBOSS_HOME/bin/standalone.sh -c standalone-full.xml
+```
+
+### Run the JMS client
+```
+mvn clean package exec:java
+```
+
+If the client execution fails, check if the issue [JBEAP-8047](https://issues.jboss.org/browse/JBEAP-8047) is fixed already in your version. 
--- src/main/java/org/jboss/test/App.java
+++ src/main/java/org/jboss/test/App.java
@@ -1,14 +1,84 @@
 package org.jboss.test;
 
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSConsumer;
+import javax.jms.JMSContext;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
 /**
  * Hello world!
- * 
+ *
  * @author Josef Cacek
  */
 public class App {
+    private static final Logger log = Logger.getLogger(App.class.getName());
+
+    // Set up all the default values
+    private static final String DEFAULT_MESSAGE = "Hello, World!";
+    private static final String DEFAULT_CONNECTION_FACTORY = "jms/RemoteConnectionFactory";
+    private static final String DEFAULT_DESTINATION = "jms/queue/test";
+    private static final String DEFAULT_MESSAGE_COUNT = "1";
+    private static final String INITIAL_CONTEXT_FACTORY = "org.wildfly.naming.client.WildFlyInitialContextFactory";
+    private static final String PROVIDER_URL = "http-remoting://127.0.0.1:8080";
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
 
+        Context namingContext = null;
+
+        try {
+
+            // Set up the namingContext for the JNDI lookup
+            final Properties env = new Properties();
+            env.put(Context.INITIAL_CONTEXT_FACTORY, INITIAL_CONTEXT_FACTORY);
+            env.put(Context.PROVIDER_URL, System.getProperty(Context.PROVIDER_URL, PROVIDER_URL));
+            namingContext = new InitialContext(env);
+
+            // Perform the JNDI lookups
+            String connectionFactoryString = System.getProperty("connection.factory", DEFAULT_CONNECTION_FACTORY);
+            log.info("Attempting to acquire connection factory \"" + connectionFactoryString + "\"");
+            ConnectionFactory connectionFactory = (ConnectionFactory) namingContext.lookup(connectionFactoryString);
+            log.info("Found connection factory \"" + connectionFactoryString + "\" in JNDI");
+
+            String destinationString = System.getProperty("destination", DEFAULT_DESTINATION);
+            log.info("Attempting to acquire destination \"" + destinationString + "\"");
+            Destination destination = (Destination) namingContext.lookup(destinationString);
+            log.info("Found destination \"" + destinationString + "\" in JNDI");
+
+            int count = Integer.parseInt(System.getProperty("message.count", DEFAULT_MESSAGE_COUNT));
+            String content = System.getProperty("message.content", DEFAULT_MESSAGE);
+
+            try (JMSContext context = connectionFactory.createContext()) {
+                log.info("Sending " + count + " messages with content: " + content);
+                // Send the specified number of messages
+                for (int i = 0; i < count; i++) {
+                    context.createProducer().send(destination, content);
+                }
+
+                // Create the JMS consumer
+                JMSConsumer consumer = context.createConsumer(destination);
+                // Then receive the same number of messages that were sent
+                for (int i = 0; i < count; i++) {
+                    String text = consumer.receiveBody(String.class, 5000);
+                    log.info("Received message with content " + text);
+                }
+            }
+        } catch (NamingException e) {
+            log.log(Level.SEVERE,"Naming problem occured",e);
+        } finally {
+            if (namingContext != null) {
+                try {
+                    namingContext.close();
+                } catch (NamingException e) {
+                    log.log(Level.SEVERE,"Naming problem occured",e);
+                }
+            }
+        }
+    }
 }
