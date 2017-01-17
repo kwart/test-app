5b6e8ae9e04ee049b39ae522c062fb8d4e40feae
Josef Cacek <josef.cacek@gmail.com>
2017-01-17 17:38:53 +0100

    JBPAPP-11257 microbenchmark

--- src/main/java/org/jboss/test/App.java
+++ src/main/java/org/jboss/test/App.java
@@ -1,14 +1,35 @@
 package org.jboss.test;
 
+import org.jboss.security.identity.plugins.SimpleRole;
+import org.jboss.security.identity.plugins.SimpleRoleGroup;
+import org.openjdk.jmh.annotations.Benchmark;
+
 /**
- * Hello world!
- * 
  * @author Josef Cacek
  */
 public class App {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    private static final SimpleRoleGroup GROUP = createSimpleRoleGroup();
+
+    @Benchmark
+    public static void fillRoles() {
+        createSimpleRoleGroup();
+    }
+
+    @Benchmark
+    public static void retrieveRoles() {
+        if (GROUP.getRoles().size()<200) {
+            throw new RuntimeException();
+        }
+    }
+
+    private static SimpleRoleGroup createSimpleRoleGroup() {
+        SimpleRoleGroup group = new SimpleRoleGroup("Roles");
+        for (int i = 0; i < 100; i++) {
+            group.addRole(new SimpleRole("test-" + i));
+            group.addRole(new SimpleRole("role-" + (100 - i)));
+        }
+        return group;
     }
 
 }
