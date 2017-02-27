c03a468fbab61c9e55235d836a8be4914d253392
Josef Cacek <josef.cacek@gmail.com>
2017-02-27 11:54:06 +0100

    Make some issues in the code.

--- src/main/java/org/jboss/test/App.java
+++ src/main/java/org/jboss/test/App.java
@@ -1,14 +1,24 @@
 package org.jboss.test;
 
+import java.io.IOException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpVersion;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
 /**
  * Hello world!
- * 
+ *
  * @author Josef Cacek
  */
 public class App {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+    	System.out.println("Hello world");
+    	new IOException("I wanna fail");
+    	System.out.println("Dead code here?");
     }
 
 }
