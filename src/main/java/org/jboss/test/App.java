49287d0d951f541d6dce413b1e78f7fa99ac0282
Josef Cacek <josef.cacek@gmail.com>
2017-06-21 14:35:10 +0200

    dev

--- src/main/java/org/jboss/test/App.java
+++ src/main/java/org/jboss/test/App.java
@@ -1,14 +1,22 @@
 package org.jboss.test;
 
+import org.wildfly.security.auth.client.AuthenticationConfiguration;
+import org.wildfly.security.auth.client.AuthenticationContext;
+import org.wildfly.security.auth.client.MatchRule;
+
 /**
  * Hello world!
- * 
+ *
  * @author Josef Cacek
  */
 public class App {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+	public static void main(String[] args) {
+		AuthenticationContext.empty()
+				.with(MatchRule.ALL, AuthenticationConfiguration.EMPTY.useName("admin").usePassword("admin"))
+				.run(() -> {
+					System.out.println("Hello");
+				});
+	}
 
 }
