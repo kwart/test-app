4905b2563fd8d93c0d516680f14195f615ee4743
Josef Cacek <josef.cacek@gmail.com>
2017-08-24 14:53:40 +0200

    sonarcloud playground

--- src/test/java/org/jboss/test/AppTest.java
+++ src/test/java/org/jboss/test/AppTest.java
@@ -1,20 +1,24 @@
 package org.jboss.test;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 /**
  * A AppTest.
- * 
+ *
  * @author Josef Cacek
  */
 public class AppTest {
 
 	@Test
 	public void test() {
-		assertTrue(true);
-		// fail("Not yet implemented");
+		assertEquals("Unexpected greeting", "Hello World!", App.sayHello());
+	}
+
+	@Test
+	public void testNullToMain() {
+		App.main(null);
 	}
 
 }
