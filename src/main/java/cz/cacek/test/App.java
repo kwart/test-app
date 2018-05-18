bb6c619a60f51745bfd6bba4d57cdd0e0eee46d8
Josef Cacek <josef.cacek@gmail.com>
2018-05-18 18:01:28 +0200

    Generate <whitelist> element with <package>-s containing Serializable classes

--- src/main/java/cz/cacek/test/App.java
+++ src/main/java/cz/cacek/test/App.java
@@ -1,19 +1,56 @@
 package cz.cacek.test;
 
-import com.hazelcast.core.Hazelcast;
-import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.instance.HazelcastInstanceFactory;
+import java.io.File;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.SortedSet;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
+
+import org.clapper.util.classutil.ClassFilter;
+import org.clapper.util.classutil.ClassFinder;
+import org.clapper.util.classutil.ClassInfo;
+import org.clapper.util.classutil.SubclassClassFilter;
 
 /**
- * Hazelcast Hello world!
+ * Hello world!
  */
 public class App {
 
     public static void main(String[] args) {
-        try {
-            HazelcastInstance hz = Hazelcast.newHazelcastInstance();
-        } finally {
-            HazelcastInstanceFactory.terminateAll();
+        ClassFinder finder = new ClassFinder();
+        if (args != null)
+            for (String arg : args)
+                finder.add(new File(arg));
+        StringTokenizer tok = new StringTokenizer(System.getProperty("sun.boot.class.path"), File.pathSeparator);
+        while (tok.hasMoreTokens())
+            finder.add(new File(tok.nextToken()));
+        finder.addClassPath();
+
+        ClassFilter filter = new SubclassClassFilter(Serializable.class);
+
+        Collection<ClassInfo> foundClasses = new ArrayList<ClassInfo>();
+        finder.findClasses(foundClasses, filter);
+
+        SortedSet<String> packageNames = new TreeSet<>();
+
+        for (ClassInfo classInfo : foundClasses) {
+            String className = classInfo.getClassName();
+            if (className.startsWith("cz.cacek."))
+                continue;
+            int dotPosition = className.lastIndexOf(".");
+            if (dotPosition > 0) {
+                packageNames.add(className.substring(0, dotPosition));
+            }
         }
+
+        
+        System.out.println("<whitelist>");
+        for (String pkg : packageNames) {
+            System.out.println("  <package>" + pkg + "</package>");
+        }
+        System.out.println("</whitelist>");
     }
 }
