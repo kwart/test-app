36a881995f381eca06287da62d7702637cf8748c
Josef Cacek <josef.cacek@gmail.com>
2018-07-13 07:08:11 +0200

    dev

--- src/main/java/cz/cacek/test/App.java
+++ src/main/java/cz/cacek/test/App.java
@@ -1,7 +1,9 @@
 package cz.cacek.test;
 
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.config.Config;
+import com.hazelcast.config.NetworkConfig;
 import com.hazelcast.core.Hazelcast;
-import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.instance.HazelcastInstanceFactory;
 
 /**
@@ -10,10 +12,25 @@ import com.hazelcast.instance.HazelcastInstanceFactory;
 public class App {
 
     public static void main(String[] args) {
-        try {
-            HazelcastInstance hz = Hazelcast.newHazelcastInstance();
-        } finally {
-            HazelcastInstanceFactory.terminateAll();
+        System.out.println(System.getProperty("java.version"));
+        Config config = new Config();
+        
+        NetworkConfig networkConfig = config.getNetworkConfig();
+        networkConfig.getInterfaces().addInterface("127.0.0.1");
+        networkConfig.getJoin().getMulticastConfig().setEnabled(false);
+        networkConfig.getJoin().getTcpIpConfig().setEnabled(true).addMember("127.0.0.1");
+        long time = System.currentTimeMillis();
+        for (int i=0; i<20; i++) {
+            try {
+                Hazelcast.newHazelcastInstance(config);
+                Hazelcast.newHazelcastInstance(config);
+                HazelcastClient.newHazelcastClient();
+            } finally {
+                HazelcastClient.shutdownAll();
+                HazelcastInstanceFactory.terminateAll();
+            }
         }
+        time = System.currentTimeMillis() - time;
+        System.out.println("Total time(ms): " + time);
     }
 }
