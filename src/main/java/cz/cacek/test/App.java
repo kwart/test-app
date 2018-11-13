dff73a3003ab170093e7875a7ada690140af321c
Josef Cacek <josef.cacek@gmail.com>
2018-11-13 15:58:00 +0100

    dev

--- src/main/java/cz/cacek/test/App.java
+++ src/main/java/cz/cacek/test/App.java
@@ -1,5 +1,11 @@
 package cz.cacek.test;
 
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.client.config.ClientNetworkConfig;
+import com.hazelcast.config.Config;
+import com.hazelcast.config.DiscoveryStrategyConfig;
+import com.hazelcast.config.JoinConfig;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.instance.HazelcastInstanceFactory;
@@ -11,8 +17,23 @@ public class App {
 
     public static void main(String[] args) {
         try {
-            HazelcastInstance hz = Hazelcast.newHazelcastInstance();
+            Config config = new Config();
+//            config.setProperty("hazelcast.discovery.enabled", "true");
+//            JoinConfig joinConfig = config.getNetworkConfig().getJoin();
+//            joinConfig.getMulticastConfig().setEnabled(false);
+//            DiscoveryStrategyConfig discoveryStrategyConfig = new DiscoveryStrategyConfig("com.hazelcast.eureka.one.EurekaOneDiscoveryStrategy");
+//            discoveryStrategyConfig.addProperty("use-classpath-eureka-client-props", "false");
+//            discoveryStrategyConfig.addProperty("shouldUseDns", "false");
+//            discoveryStrategyConfig.addProperty("serviceUrl.default", "http://localhost:8080/eureka/v2/");
+//            joinConfig.getDiscoveryConfig().addDiscoveryStrategyConfig(discoveryStrategyConfig);
+            HazelcastInstance hz = Hazelcast.newHazelcastInstance(config);
+            ClientConfig clientConfig = new ClientConfig();
+//            ClientNetworkConfig networkConfig = clientConfig.getNetworkConfig();
+//            networkConfig.getAddresses().clear();
+//            networkConfig.getDiscoveryConfig().addDiscoveryStrategyConfig(discoveryStrategyConfig);
+            HazelcastInstance client = HazelcastClient.newHazelcastClient(clientConfig);
         } finally {
+            HazelcastClient.shutdownAll();
             HazelcastInstanceFactory.terminateAll();
         }
     }
