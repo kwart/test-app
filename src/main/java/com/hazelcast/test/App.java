80ce1a9d7101dd9d171a53993afe63320603b157
Josef Cacek <josef.cacek@gmail.com>
2019-06-17 09:28:07 +0200

    https://stackoverflow.com/questions/56607144/why-does-hazelcast-drag-performance-although-when-not-running

--- src/main/java/com/hazelcast/test/App.java
+++ src/main/java/com/hazelcast/test/App.java
@@ -1,84 +1,39 @@
 package com.hazelcast.test;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.Level;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
-import org.openjdk.jmh.annotations.TearDown;
-import org.openjdk.jmh.annotations.Threads;
+import org.openjdk.jmh.infra.Blackhole;
 
-import com.hazelcast.bitset.BitSetService;
-import com.hazelcast.config.Config;
 import com.hazelcast.core.Hazelcast;
-import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.core.IBitSet;
-import com.hazelcast.core.ISet;
 
-/**
- * @author Josef Cacek
- */
 public class App {
 
     @State(Scope.Benchmark)
     public static class ExecutionPlan {
 
-//        @Param({ "10", "1000", "1000000" })
-        public int aclLength;
-
-        public HazelcastInstance hz;
-
         @Setup(Level.Trial)
         public void setUp() {
-            Config config = new Config();
-            config.getNetworkConfig().getJoin().getMulticastConfig().setEnabled(false);
-            hz = Hazelcast.newHazelcastInstance(config);
-            for (int i=1; i<5; i++) {
-                String name = "acl"+i;
-                ISet<Integer> iset = hz.getSet(name);
-                IBitSet bitSet = hz.getBitSet(name);
-                for (int j=0; j<1024; j++) {
-                    if ((j % (i+1))==0) {
-                        iset.add(j);
-                        bitSet.set(j);
-                    }
-                }
-            }
-        }
-
-        @Setup(Level.Iteration)
-        public void setUpIt() {
-            String name = "acl0";
-            ISet<Integer> iset = hz.getSet(name);
-            IBitSet bitSet = hz.getBitSet(name);
-            for (int j=0; j<1024; j++) {
-                iset.add(j);
-                bitSet.set(j);
-            }
-        }
-
-        @TearDown(Level.Trial)
-        public void tearDown() {
-            hz.getLifecycleService().terminate();
+            Hazelcast.newHazelcastInstance().shutdown();
         }
     }
 
 
     @Benchmark
-    @Threads(3)
-    public static void setBasedAcl(ExecutionPlan plan) {
-        ISet set = plan.hz.getSet("acl0");
-        for (int i=1; i<5; i++) {
-            set.retainAll(plan.hz.getSet("acl"+i));
+    public static void test(ExecutionPlan plan, Blackhole bh) throws Exception {
+        long[] numbers = new long[2<<20];
+        for (int i=0; i<numbers.length; i++) {
+            numbers[i] = i;
         }
-    }
-
-    @Benchmark
-    @Threads(3)
-    public static void bitSetBasedAcl(ExecutionPlan plan) {
-        IBitSet bitSet = plan.hz.getBitSet("acl0");
-        for (int i=1; i<5; i++) {
-            bitSet.and("acl"+i);
+        for (int i=0; i<numbers.length; i++) {
+            byte[] bytes = Long.toString(numbers[i]).getBytes("UTF-8");
+            bh.consume(MessageDigest.getInstance("SHA-1").digest(bytes));
         }
     }
 }
