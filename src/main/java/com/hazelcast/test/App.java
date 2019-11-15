2bbb7bc295ffdd9ce7875519b46c711349b689de
Josef Cacek <josef.cacek@gmail.com>
2019-11-15 11:25:33 +0100

    HZ 4

--- src/main/java/com/hazelcast/test/App.java
+++ src/main/java/com/hazelcast/test/App.java
@@ -1,13 +1,23 @@
 package com.hazelcast.test;
 
-import static java.nio.charset.StandardCharsets.US_ASCII;
+import static com.hazelcast.client.impl.protocol.ClientMessage.IS_FINAL_FLAG;
+import static com.hazelcast.client.impl.protocol.ClientMessage.RESPONSE_BACKUP_ACKS_FIELD_OFFSET;
+import static com.hazelcast.client.impl.protocol.ClientMessage.SIZE_OF_FRAME_LENGTH_AND_FLAGS;
+import static com.hazelcast.client.impl.protocol.codec.builtin.FixedSizeTypesCodec.INT_SIZE_IN_BYTES;
+import static com.hazelcast.client.impl.protocol.codec.builtin.FixedSizeTypesCodec.decodeByte;
+import static com.hazelcast.internal.nio.IOUtil.readFully;
+import static com.hazelcast.internal.nio.Protocols.CLIENT_BINARY;
+import static com.hazelcast.internal.util.StringUtil.UTF8_CHARSET;
+import static java.util.Collections.emptyList;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+import java.util.UUID;
 
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.Level;
@@ -20,11 +30,10 @@ import org.openjdk.jmh.annotations.Threads;
 import com.hazelcast.client.impl.protocol.AuthenticationStatus;
 import com.hazelcast.client.impl.protocol.ClientMessage;
 import com.hazelcast.client.impl.protocol.codec.ClientAuthenticationCodec;
-import com.hazelcast.client.impl.protocol.constants.ResponseMessageConst;
 import com.hazelcast.config.Config;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.nio.IOUtil;
+import com.hazelcast.internal.nio.Bits;
 
 /**
  * @author Josef Cacek
@@ -60,7 +69,7 @@ public class App {
         try (Socket socket = new Socket("127.0.0.1", 5701)) {
             // socket.setSoTimeout(5000);
             try (OutputStream os = socket.getOutputStream(); InputStream is = socket.getInputStream()) {
-                os.write("CB2".getBytes(US_ASCII));
+                os.write(CLIENT_BINARY.getBytes(UTF8_CHARSET));
                 os.write(AUTHN_REQUEST);
                 readResponse(is);
             }
@@ -68,44 +77,41 @@ public class App {
     }
 
     private static void readResponse(InputStream is) throws IOException {
-        ByteBuffer responseBuffer = ByteBuffer.allocate(23);
-        responseBuffer.order(ByteOrder.LITTLE_ENDIAN);
-        IOUtil.readFully(is, responseBuffer.array());
-        // frame size
-        responseBuffer.getInt();
-        // version
-        responseBuffer.get();
-        // Flags
-        responseBuffer.get();
-        // Type - Generic.AuthenticationResponse
-        short responseType = responseBuffer.getShort();
-        if (responseType != ResponseMessageConst.AUTHENTICATION) {
+        ByteBuffer frameSizeBuffer = ByteBuffer.allocate(SIZE_OF_FRAME_LENGTH_AND_FLAGS);
+        frameSizeBuffer.order(ByteOrder.LITTLE_ENDIAN);
+        readFully(is, frameSizeBuffer.array());
+        int frameSize = frameSizeBuffer.getInt();
+//        int flags = frameSizeBuffer.getShort() & 0xffff;
+        byte[] content = new byte[frameSize - SIZE_OF_FRAME_LENGTH_AND_FLAGS];
+        readFully(is, content);
+        int responseType = Bits.readIntL(content, ClientMessage.TYPE_FIELD_OFFSET);
+        if (responseType != ClientAuthenticationCodec.RESPONSE_MESSAGE_TYPE) {
             throw new IllegalStateException("Wrong response type: " + responseType);
         }
-        // Correlation ID
-        responseBuffer.getLong();
-        // Partition ID (-1)
-        responseBuffer.getInt();
-        // Data Offset
-        responseBuffer.getShort();
-
-        // Authentication status
-        byte authnStatus = responseBuffer.get();
+        int statusIdx = RESPONSE_BACKUP_ACKS_FIELD_OFFSET + INT_SIZE_IN_BYTES;
+        byte authnStatus = decodeByte(content, statusIdx);
         if (authnStatus != AuthenticationStatus.AUTHENTICATED.getId()) {
             throw new IllegalStateException("Wrong authentication status: " + authnStatus);
         }
     }
 
     private static ClientMessage createAuthenticationMessage() {
-        return ClientAuthenticationCodec.encodeRequest("dev", "dev-pass", null, null, true, "xxx", (byte) 1);
+        return ClientAuthenticationCodec.encodeRequest("dev", null, null, UUID.randomUUID(), "FOO",
+                (byte) 1, "clientName", "xxx", emptyList(), -1, null);
     }
 
     private static byte[] createRequest() {
-        ByteBuffer messageBuffer = ByteBuffer.allocateDirect(1000);
-        messageBuffer.order(ByteOrder.LITTLE_ENDIAN);
-        ClientMessage msg = createAuthenticationMessage().addFlag(ClientMessage.BEGIN_AND_END_FLAGS);
-        msg.writeTo(messageBuffer);
-        return byteBufferToBytes(messageBuffer);
+        ClientMessage msg = createAuthenticationMessage();
+        try (ByteArrayOutputStream os = new ByteArrayOutputStream()) {
+            for (ClientMessage.ForwardFrameIterator it = msg.frameIterator(); it.hasNext(); ) {
+                ClientMessage.Frame frame = it.next();
+                os.write(frameAsBytes(frame, !it.hasNext()));
+            }
+            os.flush();
+            return os.toByteArray();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private static byte[] byteBufferToBytes(ByteBuffer buffer) {
@@ -114,4 +120,20 @@ public class App {
         buffer.get(requestBytes);
         return requestBytes;
     }
+
+    private static byte[] frameAsBytes(ClientMessage.Frame frame, boolean isLastFrame) {
+        byte[] content = frame.content != null ? frame.content : new byte[0];
+        int frameSize = content.length + SIZE_OF_FRAME_LENGTH_AND_FLAGS;
+        ByteBuffer buffer = ByteBuffer.allocateDirect(frameSize);
+        buffer.order(ByteOrder.LITTLE_ENDIAN);
+        buffer.putInt(frameSize);
+        if (!isLastFrame) {
+            buffer.putShort((short) frame.flags);
+        } else {
+            buffer.putShort((short) (frame.flags | IS_FINAL_FLAG));
+        }
+        buffer.put(content);
+        return byteBufferToBytes(buffer);
+    }
+
 }
