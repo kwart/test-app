dd5d2520d6246f9cc6b95eca84bf258e7e17d13a
Josef Cacek <josef.cacek@gmail.com>
2022-02-03 10:15:01 +0100

    Devel release

--- src/main/java/cz/cacek/test/App.java
+++ src/main/java/cz/cacek/test/App.java
@@ -3,6 +3,7 @@ package cz.cacek.test;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.instance.impl.HazelcastInstanceFactory;
+import com.hazelcast.map.IMap;
 
 /**
  * The App!
@@ -11,8 +12,14 @@ public class App {
 
     public static void main(String[] args) {
         System.setProperty("hazelcast.logging.type", "log4j2");
+//        System.setProperty("hazelcast.enterprise.license.key", "");
         try {
             HazelcastInstance hz = Hazelcast.newHazelcastInstance();
+            IMap<String, String> map = hz.getMap("test");
+            map.put("key", "value");
+            if ("value".equals(hz.getMap("key"))) {
+                System.out.println("OK");
+            }
         } finally {
             HazelcastInstanceFactory.terminateAll();
         }
