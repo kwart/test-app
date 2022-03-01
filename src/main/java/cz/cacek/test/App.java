42dcfc5571ae6c4cfabd9a35b4cf06efc59a82b9
Josef Cacek <josef.cacek@gmail.com>
2022-03-01 11:12:22 +0100

    Print emoji

--- src/main/java/cz/cacek/test/App.java
+++ src/main/java/cz/cacek/test/App.java
@@ -1,9 +1,7 @@
 package cz.cacek.test;
 
-import com.hazelcast.core.Hazelcast;
-import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.instance.impl.HazelcastInstanceFactory;
-import com.hazelcast.map.IMap;
+import java.nio.charset.Charset;
+import java.util.Map.Entry;
 
 /**
  * The App!
@@ -11,17 +9,11 @@ import com.hazelcast.map.IMap;
 public class App {
 
     public static void main(String[] args) {
-        System.setProperty("hazelcast.logging.type", "log4j2");
-//        System.setProperty("hazelcast.enterprise.license.key", "");
-        try {
-            HazelcastInstance hz = Hazelcast.newHazelcastInstance();
-            IMap<String, String> map = hz.getMap("test");
-            map.put("key", "value");
-            if ("value".equals(hz.getMap("key"))) {
-                System.out.println("OK");
-            }
-        } finally {
-            HazelcastInstanceFactory.terminateAll();
+        for (Entry e:System.getProperties().entrySet()) {
+            System.out.println(e.getKey() + ": " + e.getValue());
         }
+        System.out.println();
+        System.out.println("Default charset: " + Charset.defaultCharset());
+        System.out.println("See 🔒 security");
     }
 }
