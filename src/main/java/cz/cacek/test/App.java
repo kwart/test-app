ac498d1941360dad69925e9b586fb4cc3339c997
Josef Cacek <josef.cacek@gmail.com>
2022-05-16 15:40:52 +0200

    dev

--- src/main/java/cz/cacek/test/App.java
+++ src/main/java/cz/cacek/test/App.java
@@ -1,27 +1,59 @@
 package cz.cacek.test;
 
-import com.hazelcast.core.Hazelcast;
-import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.instance.impl.HazelcastInstanceFactory;
-import com.hazelcast.map.IMap;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+
+import com.nike.riposte.server.Server;
+import com.nike.riposte.server.config.ServerConfig;
+import com.nike.riposte.server.http.Endpoint;
+import com.nike.riposte.server.http.RequestInfo;
+import com.nike.riposte.server.http.ResponseInfo;
+import com.nike.riposte.server.http.StandardEndpoint;
+import com.nike.riposte.util.Matcher;
+
+import io.netty.channel.ChannelHandlerContext;
 
 /**
  * The App!
  */
 public class App {
 
-    public static void main(String[] args) {
-        System.setProperty("hazelcast.logging.type", "log4j2");
-//        System.setProperty("hazelcast.enterprise.license.key", "");
-        try {
-            HazelcastInstance hz = Hazelcast.newHazelcastInstance();
-            IMap<String, String> map = hz.getMap("test");
-            map.put("key", "value");
-            if ("value".equals(hz.getMap("key"))) {
-                System.out.println("OK");
-            }
-        } finally {
-            HazelcastInstanceFactory.terminateAll();
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(new AppServerConfig());
+        server.startup();
+    }
+
+    public static class AppServerConfig implements ServerConfig {
+        private final Collection<Endpoint<?>> endpoints = Collections.singleton(new HelloWorldEndpoint());
+
+        @Override
+        public Collection<Endpoint<?>> appEndpoints() {
+            return endpoints;
+        }
+
+        @Override
+        public boolean isEndpointsUseSsl() {
+            return true;
+        }
+    }
+
+    public static class HelloWorldEndpoint extends StandardEndpoint<Void, String> {
+        @Override
+        public Matcher requestMatcher() {
+            return Matcher.match("/hello");
+        }
+
+        @Override
+        public CompletableFuture<ResponseInfo<String>> execute(RequestInfo<Void> request,
+                                                               Executor longRunningTaskExecutor,
+                                                               ChannelHandlerContext ctx) {
+            return CompletableFuture.completedFuture(
+                ResponseInfo.newBuilder("Hello, world!")
+                            .withDesiredContentWriterMimeType("text/plain")
+                            .build()
+            );
         }
     }
 }
