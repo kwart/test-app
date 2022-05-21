0f53b3ecb1d6ea3b10ce829180bde41ecc1c50b7
Josef Cacek <josef.cacek@gmail.com>
2022-05-21 20:54:11 +0200

    dev

--- src/main/java/cz/cacek/test/LiteMemberApp.java
+++ src/main/java/cz/cacek/test/LiteMemberApp.java
@@ -1,23 +1,25 @@
 package cz.cacek.test;
 
-import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
-import java.nio.channels.SocketChannel;
 import java.util.Collections;
+import java.util.Date;
 import java.util.UUID;
-import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 import com.hazelcast.client.impl.ClientEngine;
+import com.hazelcast.client.impl.protocol.AuthenticationStatus;
 import com.hazelcast.client.impl.protocol.ClientMessage;
 import com.hazelcast.client.impl.protocol.codec.ClientAuthenticationCodec;
+import com.hazelcast.client.impl.protocol.codec.ClientAuthenticationCodec.ResponseParameters;
 import com.hazelcast.client.impl.protocol.codec.MapPutCodec;
-import com.hazelcast.client.impl.protocol.task.AuthenticationBaseMessageTask;
-import com.hazelcast.cluster.Address;
 import com.hazelcast.config.Config;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.instance.EndpointQualifier;
+import com.hazelcast.instance.impl.HazelcastInstanceFactory;
 import com.hazelcast.instance.impl.HazelcastInstanceImpl;
 import com.hazelcast.instance.impl.HazelcastInstanceProxy;
 import com.hazelcast.internal.networking.ChannelOptions;
@@ -25,12 +27,9 @@ import com.hazelcast.internal.networking.InboundPipeline;
 import com.hazelcast.internal.networking.OutboundFrame;
 import com.hazelcast.internal.networking.OutboundPipeline;
 import com.hazelcast.internal.networking.nio.AbstractChannel;
-import com.hazelcast.internal.nio.Connection;
 import com.hazelcast.internal.nio.ConnectionLifecycleListener;
 import com.hazelcast.internal.serialization.Data;
 import com.hazelcast.internal.serialization.InternalSerializationService;
-import com.hazelcast.internal.serialization.impl.HeapData;
-import com.hazelcast.internal.server.ServerConnectionManager;
 import com.hazelcast.internal.server.tcp.TcpServerConnection;
 import com.hazelcast.internal.server.tcp.TcpServerConnectionManager;
 import com.hazelcast.partition.PartitionService;
@@ -38,45 +37,76 @@ import com.hazelcast.partition.PartitionService;
 /**
  * The App!
  */
-public class LiteMemberApp {
+public class LiteMemberApp implements Runnable {
 
-//    private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger("com.hazelcast");
-//    static {
-//        java.util.logging.ConsoleHandler ch = new java.util.logging.ConsoleHandler();
-//        ch.setLevel(java.util.logging.Level.ALL);
-//        logger.addHandler(ch);
-//        logger.setLevel(java.util.logging.Level.ALL);
-//    }
+    // private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger("com.hazelcast");
+    // static {
+    // java.util.logging.ConsoleHandler ch = new java.util.logging.ConsoleHandler();
+    // ch.setLevel(java.util.logging.Level.ALL);
+    // logger.addHandler(ch);
+    // logger.setLevel(java.util.logging.Level.ALL);
+    // }
+
+    final BlockingQueue<ClientMessage> messageQueue = new ArrayBlockingQueue<ClientMessage>(100);
 
     public static void main(String[] args) throws Exception {
-        Config config = new Config().setLiteMember(true);
-        HazelcastInstance hz = Hazelcast.newHazelcastInstance(config);
-        HazelcastInstanceImpl hzOrig = ((HazelcastInstanceProxy) hz).getOriginal();
-        TcpServerConnectionManager cm = (TcpServerConnectionManager) hzOrig.node.server
-                .getConnectionManager(EndpointQualifier.CLIENT);
-        PartitionService ps = hzOrig.getPartitionService();
-        TcpServerConnection connection = new TcpServerConnection(cm, new ConnectionLifecycleListener<TcpServerConnection>() {
+        new LiteMemberApp().run();
+    }
 
-            @Override
-            public void onConnectionClose(TcpServerConnection connection, Throwable t, boolean silent) {
-                if (t != null) {
-                    t.printStackTrace();
-                }
+    public void run() {
+        try {
+            Config config = new Config().setLiteMember(true);
+            HazelcastInstance hz = Hazelcast.newHazelcastInstance(config);
+            HazelcastInstanceImpl hzOrig = ((HazelcastInstanceProxy) hz).getOriginal();
+            TcpServerConnectionManager cm = (TcpServerConnectionManager) hzOrig.node.server
+                    .getConnectionManager(EndpointQualifier.CLIENT);
+            PartitionService ps = hzOrig.getPartitionService();
+            TcpServerConnection connection = new TcpServerConnection(cm,
+                    new ConnectionLifecycleListener<TcpServerConnection>() {
+
+                        @Override
+                        public void onConnectionClose(TcpServerConnection connection, Throwable t, boolean silent) {
+                            if (t != null) {
+                                t.printStackTrace();
+                            }
+                        }
+                    }, 0, new MyChannel(), true);
+            ClientEngine clientEngine = getClientEngine(hz);
+            InternalSerializationService ss = getSerializationService(hz);
+            UUID uuid = UUID.randomUUID();
+            ClientMessage authn = ClientAuthenticationCodec.encodeRequest("dev", null, null, uuid, "test", ss.getVersion(),
+                    "xx", "client", Collections.emptyList());
+            authn.setConnection(connection);
+            clientEngine.accept(authn);
+            ClientMessage resp = waitForResponse(ClientAuthenticationCodec.RESPONSE_MESSAGE_TYPE);
+            ResponseParameters respParams = ClientAuthenticationCodec.decodeResponse(resp);
+            if (respParams.status != AuthenticationStatus.AUTHENTICATED.getId()) {
+                throw new RuntimeException("Authentication failed");
             }
-        }, 0, new MyChannel(), true);
-        ClientEngine clientEngine = getClientEngine(hz);
-        InternalSerializationService ss = getSerializationService(hz);
-        UUID uuid = UUID.randomUUID();
-        ClientMessage authn = ClientAuthenticationCodec.encodeRequest("dev", null, null, uuid, "test", ss.getVersion(),"xx", "client",
-                Collections.emptyList());
-        authn.setConnection(connection);
-        clientEngine.accept(authn);
-        Data keyData = ss.toData("key");
-        ClientMessage message = MapPutCodec.encodeRequest("test", keyData, ss.toData("value"), 0, 0);
-        message.setConnection(connection);
-        message.setPartitionId(ps.getPartition(keyData).getPartitionId());
-        clientEngine.accept(message);
-        System.out.println(hz.getMap("test").get("key"));
+            Data keyData = ss.toData("key");
+            ClientMessage message = MapPutCodec.encodeRequest("test", keyData, ss.toData(new Date().toString()), 0, 0);
+            message.setConnection(connection);
+            message.setPartitionId(ps.getPartition(keyData).getPartitionId());
+            clientEngine.accept(message);
+            resp = waitForResponse(MapPutCodec.RESPONSE_MESSAGE_TYPE);
+            Data putRP = MapPutCodec.decodeResponse(resp);
+            System.out.println((Object) ss.toObject(putRP));
+            System.out.println(hz.getMap("test").get("key"));
+        } finally {
+            HazelcastInstanceFactory.terminateAll();
+        }
+    }
+
+    private ClientMessage waitForResponse(int responseMessageType) {
+        ClientMessage cm = null;
+        try {
+            do {
+                cm = messageQueue.poll(30, TimeUnit.SECONDS);
+            } while (cm.getMessageType() != responseMessageType);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return cm;
     }
 
     private static InternalSerializationService getSerializationService(HazelcastInstance hz) {
@@ -87,7 +117,7 @@ public class LiteMemberApp {
         return ((HazelcastInstanceProxy) hz).getOriginal().node.clientEngine;
     }
 
-    static class MyChannel extends AbstractChannel {
+    class MyChannel extends AbstractChannel {
 
         @Override
         public SocketAddress remoteSocketAddress() {
@@ -123,12 +153,12 @@ public class LiteMemberApp {
 
         @Override
         public long lastReadTimeMillis() {
-            return System.currentTimeMillis()-10;
+            return System.currentTimeMillis() - 10;
         }
 
         @Override
         public long lastWriteTimeMillis() {
-            return System.currentTimeMillis()-10;
+            return System.currentTimeMillis() - 10;
         }
 
         @Override
@@ -141,7 +171,12 @@ public class LiteMemberApp {
         public boolean write(OutboundFrame frame) {
             System.out.println(frame);
             ClientMessage cm = (ClientMessage) frame;
-            cm.getMessageType();
+            try {
+                messageQueue.offer(cm, 1, TimeUnit.MINUTES);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                return false;
+            }
             return true;
         }
 
