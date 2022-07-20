30357b82243c10736e7efcd89a6c8f1ab75fc506
Josef Cacek <josef.cacek@gmail.com>
2022-07-20 10:56:41 +0200

    Finish the reproducer

--- src/main/java/cz/cacek/test/App.java
+++ src/main/java/cz/cacek/test/App.java
@@ -10,18 +10,38 @@ import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.instance.impl.HazelcastInstanceFactory;
 
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 public class App {
 
     public static void main(String[] args) throws Exception {
+        boolean passed = false;
         try {
             HazelcastInstance hz2 = Hazelcast.newHazelcastInstance(createConfig(5801, 5701));
             HazelcastInstance hz3 = Hazelcast.newHazelcastInstance(createConfig(5901, 5701, 5801));
-            HazelcastInstance master = Hazelcast.newHazelcastInstance(createConfig(5701));
+            HazelcastInstance hz1 = Hazelcast.newHazelcastInstance(createConfig(5701));
+            TimeUnit.SECONDS.sleep(5);
+            assertClusterSize(2, hz2, hz3);
+            assertClusterSize(1, hz1);
+
+            OkHttpClient client = new OkHttpClient();
+            RequestBody body = RequestBody.create("dev&&127.0.0.1:5901", MediaType.get("text/plain"));
+            Request request = new Request.Builder().url("http://localhost:5701/hazelcast/rest/config/tcp-ip/member-list")
+                    .post(body).build();
+            try (Response response = client.newCall(request).execute()) {
+                System.out.println("MemberList update response: " + response.body().string());
+            }
+
             TimeUnit.SECONDS.sleep(60);
-            assertClusterSize(3, master, hz2, hz3);
-            System.out.println(">>> OK");
+            assertClusterSize(3, hz1, hz2, hz3);
+            passed = true;
         } finally {
             HazelcastInstanceFactory.terminateAll();
+            System.out.println(passed ? ">>> OK" : ">>> FAILED");
         }
     }
 
@@ -31,6 +51,7 @@ public class App {
                 .setProperty("hazelcast.merge.next.run.delay.seconds", "20");
         NetworkConfig networkConfig = config.getNetworkConfig();
         networkConfig.setPortAutoIncrement(false).setPort(port);
+        networkConfig.getRestApiConfig().setEnabled(true).enableAllGroups();
         JoinConfig join = networkConfig.getJoin();
         join.getAutoDetectionConfig().setEnabled(false);
         join.getMulticastConfig().setEnabled(false);
@@ -45,8 +66,9 @@ public class App {
         for (int i = 0; i < instances.length; i++) {
             int clusterSize = instances[i].getCluster().getMembers().size();
             if (expectedSize != clusterSize) {
-                throw new RuntimeException(String.format("Cluster size is not correct. Expected: %d, actual: %d, instance index: %d",
-                        expectedSize, clusterSize, i));
+                throw new RuntimeException(
+                        String.format("Cluster size is not correct. Expected: %d, actual: %d, instance index: %d", expectedSize,
+                                clusterSize, i));
             }
         }
     }
