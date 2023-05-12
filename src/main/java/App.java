2f68f5563c9248089e0aec559771c172dfbfb77d
Josef Cacek <josef.cacek@gmail.com>
2023-05-12 11:48:18 +0200

    Set Timeouts and allow plain HTTP connections too

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,8 @@
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
-
-import javax.net.ssl.HttpsURLConnection;
 
 /**
  * The App!
@@ -15,13 +14,15 @@ public class App {
         if (args!=null && args.length>0) {
             url = args[0];
         }
-        HttpsURLConnection conn = (HttpsURLConnection) new URL(url).openConnection();
+        HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+        conn.setConnectTimeout(5000);
+        conn.setReadTimeout(5000);
         conn.setInstanceFollowRedirects(false);
         conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2");
         try (InputStream is = conn.getInputStream(); BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
-            System.out.println(conn.getHeaderFields());
+            System.out.println("HTTP headers:\n" + conn.getHeaderFields());
             String line;
-            while ((line = br.readLine())!=null) {
+            while ((line = br.readLine()) != null) {
                 System.out.println(line);
             }
         }
