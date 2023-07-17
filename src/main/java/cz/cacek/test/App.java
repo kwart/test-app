ed17e6d4601455b510832fc2e09ecf349d64422f
Josef Cacek <josef.cacek@gmail.com>
2023-07-17 16:28:08 +0200

    Simple HZ member connection check

--- src/main/java/cz/cacek/test/App.java
+++ src/main/java/cz/cacek/test/App.java
@@ -1,9 +1,9 @@
 package cz.cacek.test;
 
-import com.hazelcast.core.Hazelcast;
-import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.instance.impl.HazelcastInstanceFactory;
-import com.hazelcast.map.IMap;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 /**
  * The App!
@@ -11,17 +11,38 @@ import com.hazelcast.map.IMap;
 public class App {
 
     public static void main(String[] args) {
-        System.setProperty("hazelcast.logging.type", "log4j2");
-//        System.setProperty("hazelcast.enterprise.license.key", "");
-        try {
-            HazelcastInstance hz = Hazelcast.newHazelcastInstance();
-            IMap<String, String> map = hz.getMap("test");
-            map.put("key", "value");
-            if ("value".equals(hz.getMap("key"))) {
-                System.out.println("OK");
-            }
-        } finally {
-            HazelcastInstanceFactory.terminateAll();
+        if (args == null || args.length != 2) {
+            usage("Two parameters expected!");
         }
+        String host = args[0];
+        int port = 5701;
+        try {
+            port = Integer.parseInt(args[1]);
+        } catch (NumberFormatException nfe) {
+            usage("Port has to be a number!");
+        }
+        try (Socket socket = new Socket(host, port)) {
+            try (InputStream is = socket.getInputStream(); OutputStream os = socket.getOutputStream()) {
+                os.write("HZC".getBytes(StandardCharsets.UTF_8));
+                byte[] resp = new byte[3];
+                for (int i = 0; i < resp.length; i++) {
+                    resp[i] = (byte) is.read();
+                }
+                System.out.println(new String(resp, StandardCharsets.UTF_8));
+            } catch (Exception e) {
+                System.out.println("FAIL-IO");
+            }
+        } catch (Exception e) {
+            System.out.println("FAIL-CONNECT");
+        }
+    }
+
+    private static void usage(String msg) {
+        System.err.println(msg);
+        System.err.println();
+        System.err.println("Usage:");
+        System.err.println("\tjava -jar <appname.jar> <host> <port>");
+        System.err.println();
+        System.exit(1);
     }
 }
